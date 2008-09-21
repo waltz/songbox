29224d66a413697aee0d2deac6412765654b41d1
Christian Bryan <cbryan@Tern.(none)>
2008-09-20 22:36:23 -0400

    Connected the search method to the search button.

--- gui.rb
+++ gui.rb
@@ -1,30 +1,48 @@
 require 'seeqpod'
 
 Shoes.app(:title => 'Songbox',
-          :width => 350,
-          :height => 400,
+          :width => 450,
+          :height => 500,
           :resizable => false) do
   
-  background rgb(237, 227, 158)
-  
-  @search_box = flow do
-    background rgb(255, 93, 40)
-    edit_line(:width => 200).text = "Search"
-    button "Search"
-  end
-  
-  @search_results = flow do
-    Seeqpod.search('jay-z').each do |track|
+  def search(query = "")
+    @search_results.clear do
       flow do
-        para track.title
+        Seeqpod.search(query).each do |track|
+          flow do
+            para track.title
+            button "Download" do
+              download(track.uri, :save => 'foo.mp3') do
+                alert "Downloaded Finished"
+              end
+            end
+          end
+        end
       end
     end
   end
+            
+  background rgb(237, 227, 158)
   
+  @search_box = flow(:width => 450, :height => 50) do
+    background rgb(255, 93, 40)
+    scroll false
+    
+    @search_query = edit_line(:width => 200)
+    
+    @search_go = button("Search")
+    @search_go.click { search(@search_query.text) }
+  end
   
-  stack(:width => 350) do
+  @search_results = flow { para "Go for it punk!" }
+  
+  # Draw the interface.
+  stack do
     @search_box
     @search_results
   end
+  
 
+  
 end
+
