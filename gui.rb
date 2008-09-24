83302688012312490b3af0f153685c98313b3528
Christian Bryan <cbryan@Tern.(none)>
2008-09-23 20:55:07 -0400

    Axed the separate seeqpod class and moved all of the accessory methods over to gui.rb. Created a Songbox module to hold all of this garbage.

--- gui.rb
+++ gui.rb
@@ -1,9 +1,34 @@
-require 'seeqpod'
+require 'net/http'
+require 'uri'
+require 'rexml/document'
 
-Shoes.app(:title => 'Songbox',
-          :width => 450,
-          :height => 500,
-          :resizable => false) do
+# A Seeqpod user id so we can use their API.
+USER_ID = "92b90c39b6fa07674f7b4f1ca07fc42ec33eda87"
+
+module Songbox
+  
+  Track = Struct.new(:artist, :title, :location)
+  
+  def seeqpod_search(query, args = {})
+    query = URI.escape(query)
+    start_index = args[:start_index] || 0   # Default index is 0 unless previously set.
+    num_results = args[:num_results] || 10  # Default is 10 unless...
+    user_id = USER_ID
+    uri = "http://www.seeqpod.com/api/v0.2/#{user_id}/music/search/#{query}/#{start_index}/#{num_results}/"
+    
+    data = Net::HTTP.get(URI.parse(uri))
+    doc = REXML::Document.new(data)
+    tracks = []
+    
+    doc.root.each_element('//track') do |track|
+      location = URI.escape(track.elements[1].text)
+      artist = track.elements[3].text
+      title = track.elements[2].text
+      tracks << Track.new(artist, title, location)
+    end
+    
+    tracks
+  end
   
   def preview_widget(location)
     flow do
@@ -21,57 +46,55 @@ Shoes.app(:title => 'Songbox',
   def download_widget(location)
     flow do
       p = progress(:width => 1.0)
-      button "download" do
-        info "Started: " + File.basename(location)
+      button "Download" do
         download(location,
                  :save => File.basename(location),
-                 :progress => proc { |dl| p.fraction = dl.percent * 0.01 },
-                 :finish => proc { info "Finished: " + File.basename(location) })
+                 :progress => proc { |dl| p.fraction = dl.percent * 0.01 })
       end
     end
   end
   
-  def search(query = "")
-    @search_results.clear do
-      flow do
-        Seeqpod.search(query).each do |track|
-          info "Showing: " + track.artist.to_s + " " + track.title.to_s
-          flow do
-            para strong track.artist.to_s
-            para em track.title.to_s
-            #preview_widget(track.location)
+  # Asks Seeqpod for a list of results and formats them.
+  def render_search(query, container)
+    search_results = nil
+
+    # Do the search asynchronously.
+    search_thread = Thread.new do
+      search_results = flow do
+          seeqpod_search(query).each do |track|
             flow do
-              v = video track.location
-              v.hide
-              button("play") { v.play }
-              button("pause") { v.pause }
-              t = para ""
-              animate do
-                t.replace((v.time.to_i * 0.001).to_s)
-              end
+              para strong track.artist.to_s
+              para em track.title.to_s
+              #preview_widget(track.location)
+              download_widget(track.location)
             end
-            download_widget(track.location)
           end
-        end
       end
     end
-  end
-            
-  background white
-  
-  search_box = flow(:width => 450, :height => 50) do
-    background black
-    search_query = edit_line(:width => 200)
-    search_go = button("Search")
-    search_go.click { search(search_query.text) }
-  end
-  
-  @search_results = flow { para "Go for it punk!" }
-  
-  stack do
-    search_box
-    @search_results
+    
+    search_results
   end
   
 end
 
+Shoes.app(:title => 'Songbox', :width => 450, :height => 500, :resizable => false) do
+  
+  extend Songbox
+            
+  background white
+
+  @search_box = flow(:width => 450, :height => 50) do
+    background black
+    search_query = edit_line
+    search_button = button("Search")
+    search_button.click { render_search(search_query.text, @search_results_view) }
+  end
+  
+  @search_results_view = flow { para "Go for it punk!" }
+  
+  stack do
+    @search_box
+    @search_results_view
+  end
+  
+end
