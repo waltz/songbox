013bce12b37701c75f22a3d44aed3bd214c3c7a2
Christian Bryan <christianbryan@gmail.com>
2009-01-05 23:58:59 -0500

    Niddling fixes to the fetcher. Renders the progress circle a little more cleanly now.

--- fetcher.rb
+++ fetcher.rb
@@ -1,13 +1,14 @@
 require 'downloader'
+require 'thread'
 
 class Fetcher < Shoes::Widget
   
   def initialize(options = {})
     @url = options[:url]
     
-    @container = flow {
+    @container = flow(:height => 100, :width => 100) do
       para "Loading"
-    }
+    end
     
     @state = :idle
     idle
@@ -39,34 +40,46 @@ protected
   
   def download
     info "Download action triggered."
-    # begin
-    #   downloader = Downloader.new(@url)
-    #   downloader.download do |percent|
-    #     @container.clear do
-    #       draw_progress(percent)
-    #     end
-    #   end
-    # rescue
-    #   error
-    # end
-    @container.clear do
-      draw_progress(0.35)
+    
+    # Setup the mutex and variable.
+    percent = 0
+    percent_mutex = Mutex.new
+    
+    # Download the file and update the percent counter.
+    download_thread = Thread.new do
+      Downloader.new(@url).download do |i|
+       percent_mutex.synchronize do
+         percent = i
+       end 
+      end
+    end
+    
+    # Render the progress circle.
+    animate do
+      @container.clear do
+        percent_mutex.synchronize do
+          draw_progress(percent)
+        end
+      end
     end
   end
 
   def info(msg)
-    Shoes::info("Fetcher Widget - #{self.object_id}: #{msg}")
+    Shoes::info("Fetcher - #{self.object_id}: #{msg}")
   end
   
   def draw_progress(percent)
+    para percent.to_s
+    
     yellow = rgb(238, 238, 17)
-    stroke yellow
-    nofill
+    orange = rgb(255, 117, 24)
+
+    stroke orange
+    fill yellow
     strokewidth 5
-    start = -(HALF_PI)
-    stop = start + (TWO_PI * percent)
-    # para @percent
-    # para @i
+  
+    start = -(Shoes::HALF_PI)
+    stop = start + (Shoes::TWO_PI * percent)
     arc 50, 50, 50, 50, start, stop
   end
   
